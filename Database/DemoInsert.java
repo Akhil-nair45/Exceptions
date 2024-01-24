a9d54f57131c39976e629bf368d380eb3bbfd1e9
Akhil Nair <Akhil.Nair@einfochips.com>
2024-01-24 17:17:43 +0530

    first commit

---
+++ Database/DemoInsert.java
@@ -0,0 +1,31 @@
+package Database;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+import java.util.Scanner;
+
+public class DemoInsert {
+    public static void main(String[] args) throws Exception
+     {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int no =sc.nextInt();
+        System.out.println("Enter the Name: ");
+        String name = sc.next();
+        System.out.println("Enter the number: ");
+        int runs = sc.nextInt();
+
+        // logic for db
+        // step1 
+        class.forName(" ");
+
+        // step2
+        Statement st = sc.createStatement();
+
+        // 
+        boolean
+
+     }
+}
